b3d906f89b7f04ec2e921782b6e4a62dce523a45
reynaldo0 <reynaldooyusellino@gmail.com>
2024-04-05 09:10:57 +0700

    add new commits

---
+++ tailwind.config.js
@@ -0,0 +1,47 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.{html,js}",
+    "./index.html",
+  ],
+
+  theme: {
+    container: {
+      DEFAULT : '15px'
+    },
+    screens: {
+      sm : '640px',
+      md : '768px',
+      lg : '960px',
+      xl : '1330px',
+    },
+    extend: {
+      colors: {
+        primary: '#242a2b',
+        secondary: '#808080',
+        accent: {
+          DEFAULT: '#1cbccf',
+          secondary: '#18abbc',
+          tertiary: '#90c6cd'
+        },
+        grey: '#e8f0f1',
+      },
+      fontFamily:{
+        primary: 'Poppins',
+      },
+      boxShadow: {
+        custom1: '0px 2px 40px 0px rgba(8, 70, 70, 0.08)',
+        custom2: '0px 0px 30px 0px rgba(8, 73, 81, 0.06)',
+      },
+      backgroundImage: {
+        services: 'url(../../src/assets/img/service/bg.svg',
+        testimonials: 'url(../../src/assets/img/testimonials/bg.svg',
+        departments: 'url(../../src/assets/img/depatments/bg.svg',
+        quoteLeft: 'url(../../src/assets/icons/testimonials/quote-left.svg',
+        quoteRight: 'url(../../src/assets/icons/testimonials/quote-right.svg',
+      }
+    },
+  },
+  plugins: [],
+}
+
