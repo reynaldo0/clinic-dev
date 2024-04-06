c51b019403ab96b68a389961bb409b853152d3ab
reynaldo0 <reynaldooyusellino@gmail.com>
2024-04-06 16:30:58 +0700

    text "add new services container"

--- public/js/main.js
+++ public/js/main.js
@@ -8,11 +8,27 @@ const navClosedClass = '-left-[300px]';
 const arrowLeftClass = 'ri-arrow-left-s-line';
 const arrowRightClass = 'ri-arrow-right-s-line';
 
-closeBtn.addEventListener('click', ()=> {
-    if (mobileNav.classList.contains(navClosedClass)){
-        mobileNav.classList.toggle(navOpenedClass);
+// Function to close the navbar
+function closeNavbar() {
+    mobileNav.classList.remove(navOpenedClass);
+    mobileNav.classList.add(navClosedClass);
+    closeBtnIcon.classList.remove(arrowLeftClass);
+    closeBtnIcon.classList.add(arrowRightClass);
+}
 
+// Event listener for the close button
+closeBtn.addEventListener('click', () => {
+    if (mobileNav.classList.contains(navClosedClass)) {
+        mobileNav.classList.toggle(navOpenedClass);
         closeBtnIcon.classList.toggle(arrowLeftClass);
         closeBtnIcon.classList.toggle(arrowRightClass);
     }
-})
+});
+
+// Event listener for clicks outside the navbar
+document.addEventListener('click', (event) => {
+    const isClickedOutside = !mobileNav.contains(event.target) && !closeBtn.contains(event.target);
+    if (isClickedOutside && mobileNav.classList.contains(navOpenedClass)) {
+        closeNavbar();
+    }
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -34,12 +34,13 @@ module.exports = {
         custom2: '0px 0px 30px 0px rgba(8, 73, 81, 0.06)',
       },
       backgroundImage: {
-        services: 'url(../../src/assets/img/service/bg.svg',
-        testimonials: 'url(../../src/assets/img/testimonials/bg.svg',
-        departments: 'url(../../src/assets/img/depatments/bg.svg',
-        quoteLeft: 'url(../../src/assets/icons/testimonials/quote-left.svg',
-        quoteRight: 'url(../../src/assets/icons/testimonials/quote-right.svg',
+        services: 'url(../../public/assets/img/services/bg.svg)',
+        testimonials: 'url(../../public/assets/img/testimonials/bg.svg)',
+        departments: 'url(../../public/assets/img/departments/bg.svg)',
+        quoteLeft: 'url(../../public/assets/icons/testimonials/quote-left.svg)',
+        quoteRight: 'url(../../public/assets/icons/testimonials/quote-right.svg)',
       }
+      
     },
   },
   plugins: [],
