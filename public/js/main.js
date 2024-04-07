01331301e5f5edb504d9ed2e0486399210497ae6
reynaldo0 <reynaldooyusellino@gmail.com>
2024-04-07 20:13:54 +0700

    add new element

--- public/js/main.js
+++ public/js/main.js
@@ -1,34 +1,48 @@
-// Mobile Navbar 
-const mobileNav = document.querySelector('.mnav');
-const closeBtn = document.querySelector('.mnav_close-btn');
-const closeBtnIcon = document.querySelector('.mnav_close-btn-icon');
+// Mobile Navbar
+const mobileNav = document.querySelector(".mnav");
+const closeBtn = document.querySelector(".mnav_close-btn");
+const closeBtnIcon = document.querySelector(".mnav_close-btn-icon");
 
-const navOpenedClass = 'left-0';
-const navClosedClass = '-left-[300px]';
-const arrowLeftClass = 'ri-arrow-left-s-line';
-const arrowRightClass = 'ri-arrow-right-s-line';
+const navOpenedClass = "left-0";
+const navClosedClass = "-left-[300px]";
+const arrowLeftClass = "ri-arrow-left-s-line";
+const arrowRightClass = "ri-arrow-right-s-line";
 
 // Function to close the navbar
 function closeNavbar() {
-    mobileNav.classList.remove(navOpenedClass);
-    mobileNav.classList.add(navClosedClass);
-    closeBtnIcon.classList.remove(arrowLeftClass);
-    closeBtnIcon.classList.add(arrowRightClass);
+  mobileNav.classList.remove(navOpenedClass);
+  mobileNav.classList.add(navClosedClass);
+  closeBtnIcon.classList.remove(arrowLeftClass);
+  closeBtnIcon.classList.add(arrowRightClass);
 }
 
 // Event listener for the close button
-closeBtn.addEventListener('click', () => {
-    if (mobileNav.classList.contains(navClosedClass)) {
-        mobileNav.classList.toggle(navOpenedClass);
-        closeBtnIcon.classList.toggle(arrowLeftClass);
-        closeBtnIcon.classList.toggle(arrowRightClass);
-    }
+closeBtn.addEventListener("click", () => {
+  if (mobileNav.classList.contains(navClosedClass)) {
+    mobileNav.classList.toggle(navOpenedClass);
+    closeBtnIcon.classList.toggle(arrowLeftClass);
+    closeBtnIcon.classList.toggle(arrowRightClass);
+  }
 });
 
 // Event listener for clicks outside the navbar
-document.addEventListener('click', (event) => {
-    const isClickedOutside = !mobileNav.contains(event.target) && !closeBtn.contains(event.target);
-    if (isClickedOutside && mobileNav.classList.contains(navOpenedClass)) {
-        closeNavbar();
-    }
+document.addEventListener("click", (event) => {
+  const isClickedOutside =
+    !mobileNav.contains(event.target) && !closeBtn.contains(event.target);
+  if (isClickedOutside && mobileNav.classList.contains(navOpenedClass)) {
+    closeNavbar();
+  }
 });
+// mobile navbar ends
+
+// Swiper starts
+document.addEventListener('DOMContentLoaded', function () {
+    const swiper = new Swiper(".swiper", {
+      loop: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+      },
+    });
+  });
+// Swiper ends
